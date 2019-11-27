3063af6403121abe3842f93e410026092e404a9f
student <student>
2019-11-27 16:27:58 +0100

    Bibliothek

--- D/D5/src/Bibliothek.java
+++ D/D5/src/Bibliothek.java
@@ -1,2 +1,54 @@
 public class Bibliothek {
+
+    public static void main(String[] args) throws Medium.ValidationException {
+        Zettelkasten zettelkasten = new Zettelkasten();
+
+        Buch buch = new Buch();
+        buch.setTitel("Duden 01. Die deutsche Rechtschreibung");
+        buch.setVerfasser("-");
+        buch.setVerlag("Bibliographisches Institut, Mannheim");
+        buch.setIsbn("3-411-04013-0");
+        buch.setErscheinungsjahr(2004);
+        zettelkasten.addMedium(buch);
+
+        CD cd = new CD();
+        cd.setTitel("1");
+        cd.setLabel("Apple (Bea (EMI)");
+        cd.setKuenstler("The Beatles");
+        zettelkasten.addMedium(cd);
+
+        Zeitschrift zeitschrift = new Zeitschrift();
+        zeitschrift.setTitel("Der Spiegel");
+        zeitschrift.setVolume(54);
+        zeitschrift.setNummer(6);
+        zeitschrift.setIssn("0038-7452");
+        zettelkasten.addMedium(zeitschrift);
+
+        ElektronischesMedium elektronischesMedium = new ElektronischesMedium();
+        elektronischesMedium.setTitel("Hochschule Stralsund");
+        elektronischesMedium.setUrl("http://www.hochschule-stralsund.de");
+        zettelkasten.addMedium(elektronischesMedium);
+
+        zettelkasten.sort();
+
+        for (Medium medium : zettelkasten) {
+            System.out.println(medium.calculateRepresentation());
+        }
+
+        /*try {
+            new DatabasePersistency("localhost", "lpsw").save(zettelkasten, null);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        try {
+            System.out.println("loading:");
+            Zettelkasten zkNew = new DatabasePersistency("localhost", "lpsw").load(null);
+            for (Medium medium : zkNew) {
+                System.out.println(medium.calculateRepresentation());
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }*/
+    }
 }
